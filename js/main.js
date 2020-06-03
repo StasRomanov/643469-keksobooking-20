0bade9e5e3c996d409e7c9729c98741a6daba39a
StasRomanov <stasromanov801@gmail.com>
2020-06-03 20:23:29 +0300

    create 8 random object

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 'use strict';
-var array = [];
+var hotelArrays = [];
 var hotelType = ['palace', 'flat', 'house', 'bungalo'];
 var time = ['12:00', '13:00', '14:00'];
 var featuresAll = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var photoHotel = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var map = document.querySelector('.map');
-
 var randomInteger = function (min, max) {
   var rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 };
-
-var xLocation = randomInteger(130, 630);
-var yLocation = randomInteger(130, 630);
+var xLocation = [randomInteger(130, 630), randomInteger(130, 630), randomInteger(130, 630), randomInteger(130, 630),
+  randomInteger(130, 630), randomInteger(130, 630), randomInteger(130, 630), randomInteger(130, 630)];
+var yLocation = [randomInteger(130, 630), randomInteger(130, 630), randomInteger(130, 630), randomInteger(130, 630),
+  randomInteger(130, 630), randomInteger(130, 630), randomInteger(130, 630), randomInteger(130, 630)];
 
 var randomArray = function (array) {
   var newArray = [];
@@ -25,7 +25,33 @@ var randomArray = function (array) {
   return newArray;
 };
 
-var objectHotel = {
+var getHotelInfo = function (avatar, title, address, price, type, rooms, guests,
+    checkin, checkout, features, description, photos, x, y) {
+  return {
+    author: {
+      avatar: avatar
+    },
+    offer: {
+      title: title,
+      address: address,
+      price: price,
+      type: type,
+      rooms: rooms,
+      guests: guests,
+      checkin: checkin,
+      checkout: checkout,
+      features: features,
+      description: description,
+      photos: photos
+    },
+    location: {
+      x: x,
+      y: y
+    }
+  };
+};
+
+/* var objectHotel = {
   author: {
     avatar: 'img/avatars/user0' + randomInteger(1, 8) + '.png'
   },
@@ -46,14 +72,19 @@ var objectHotel = {
     x: xLocation,
     y: yLocation
   }
-};
+};*/
 
 var arrayCreator = function () {
-  for (var i = 0; i <= 8; i++) {
-    array.splice(0, 0, objectHotel);
+  for (var i = 0; i < 8; i++) {
+    hotelArrays.splice(0, 0, getHotelInfo(
+        'img/avatars/user0' + randomInteger(1, 8) + '.png', 'hotel' + '', 'x:' +
+        xLocation[i] + ' y:' + yLocation[i], randomInteger(1000, 80000), hotelType[randomInteger(0, 3)], randomInteger(1, 6),
+        randomInteger(1, 6) * 3, time[randomInteger(0, 2)], time[randomInteger(0, 2)], randomArray(featuresAll),
+        'so looooooooooooooooooong text', randomArray(photoHotel), xLocation[i], yLocation[i]
+    ));
   }
 };
 
 map.classList.remove('map--faded');
 arrayCreator();
-console.log(array);
+console.log(hotelArrays);
