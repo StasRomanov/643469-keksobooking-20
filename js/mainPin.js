13587bdc7bf2cafc519260031671a41ec32313df
StasRomanov <stasromanov801@gmail.com>
2020-07-09 10:12:24 +0300

    B-2,1

--- js/mainPin.js
+++ js/mainPin.js
@@ -27,21 +27,21 @@
 
         window.utilData.mapPinMain.style.left = (window.utilData.mapPinMain.offsetLeft - shift.x) + 'px';
         window.utilData.mapPinMain.style.top = (window.utilData.mapPinMain.offsetTop - shift.y) + 'px';
-        window.utilData.addressInput.value = String(Math.round(window.utilData.mapPinMain.offsetLeft - shift.x + window.utilData.mapPinMain.offsetWidth / 2)) + ' '
+        window.utilData.addressInput.value = String(Math.round(window.utilData.mapPinMain.offsetLeft - shift.x + window.utilData.mapPinMain.offsetWidth / 2)) + ', '
         + String(window.utilData.mapPinMain.offsetTop - shift.y + window.utilData.mapPinMain.offsetHeight + window.utilData.MAP_PIN_TRIANGLE_HEIGHT);
 
-        while (window.utilData.mapPinMain.offsetLeft + window.utilData.mapPinMain.offsetWidth >
+        if (window.utilData.mapPinMain.offsetLeft + window.utilData.mapPinMain.offsetWidth >
         window.utilData.mapOverlay.offsetWidth) {
           window.utilData.mapPinMain.style.left = (window.utilData.mapPinMain.offsetLeft - window.utilData.MAP_SAFE_BORDER_ZONE) + 'px';
         }
-        while (window.utilData.mapPinMain.offsetTop + window.utilData.mapPinMain.offsetHeight + window.utilData.MAP_PIN_TRIANGLE_HEIGHT >
+        if (window.utilData.mapPinMain.offsetTop + window.utilData.mapPinMain.offsetHeight + window.utilData.MAP_PIN_TRIANGLE_HEIGHT >
         window.utilData.mapOverlay.offsetHeight) {
           window.utilData.mapPinMain.style.top = (window.utilData.mapPinMain.offsetTop - window.utilData.MAP_SAFE_BORDER_ZONE) + 'px';
         }
-        while (window.utilData.mapPinMain.offsetLeft < 0) {
+        if (window.utilData.mapPinMain.offsetLeft < 0) {
           window.utilData.mapPinMain.style.left = (window.utilData.mapPinMain.offsetLeft + window.utilData.MAP_SAFE_BORDER_ZONE) + 'px';
         }
-        while (window.utilData.mapPinMain.offsetTop - window.utilData.SKY_HEIGHT + window.utilData.mapPinMain.offsetHeight +
+        if (window.utilData.mapPinMain.offsetTop - window.utilData.SKY_HEIGHT + window.utilData.mapPinMain.offsetHeight +
         window.utilData.MAP_PIN_TRIANGLE_HEIGHT < 0) {
           window.utilData.mapPinMain.style.top = (window.utilData.mapPinMain.offsetTop + window.utilData.MAP_SAFE_BORDER_ZONE) + 'px';
         }
