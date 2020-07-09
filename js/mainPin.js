19ae76a041f2c419c5f8b62a4d076fe7d28e3d0b
StasRomanov <stasromanov801@gmail.com>
2020-07-09 10:21:36 +0300

    B-1 fix

--- js/mainPin.js
+++ js/mainPin.js
@@ -35,18 +35,18 @@
         window.utilData.addressInput.value = String(Math.round(window.utilData.mapPinMain.offsetLeft - shift.x + window.utilData.mapPinMain.offsetWidth / 2)) + ', '
         + String(window.utilData.mapPinMain.offsetTop - shift.y + window.utilData.mapPinMain.offsetHeight + window.utilData.MAP_PIN_TRIANGLE_HEIGHT);
 
-        if (window.utilData.mapPinMain.offsetLeft + window.utilData.mapPinMain.offsetWidth >
+        while (window.utilData.mapPinMain.offsetLeft + window.utilData.mapPinMain.offsetWidth >
         window.utilData.mapOverlay.offsetWidth) {
           window.utilData.mapPinMain.style.left = (window.utilData.mapPinMain.offsetLeft - window.utilData.MAP_SAFE_BORDER_ZONE) + 'px';
         }
-        if (window.utilData.mapPinMain.offsetTop + window.utilData.mapPinMain.offsetHeight + window.utilData.MAP_PIN_TRIANGLE_HEIGHT >=
+        while (window.utilData.mapPinMain.offsetTop + window.utilData.mapPinMain.offsetHeight + window.utilData.MAP_PIN_TRIANGLE_HEIGHT >=
           safeZone.bottom) {
           window.utilData.mapPinMain.style.top = (window.utilData.mapPinMain.offsetTop - window.utilData.MAP_SAFE_BORDER_ZONE) + 'px';
         }
-        if (window.utilData.mapPinMain.offsetLeft < 0) {
+        while (window.utilData.mapPinMain.offsetLeft < 0) {
           window.utilData.mapPinMain.style.left = (window.utilData.mapPinMain.offsetLeft + window.utilData.MAP_SAFE_BORDER_ZONE) + 'px';
         }
-        if (window.utilData.mapPinMain.offsetTop + window.utilData.mapPinMain.offsetHeight +
+        while (window.utilData.mapPinMain.offsetTop + window.utilData.mapPinMain.offsetHeight +
         window.utilData.MAP_PIN_TRIANGLE_HEIGHT <= safeZone.top) {
           window.utilData.mapPinMain.style.top = (window.utilData.mapPinMain.offsetTop + window.utilData.MAP_SAFE_BORDER_ZONE) + 'px';
         }
