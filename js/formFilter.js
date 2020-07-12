b513744dd7325e18ae0844ba05d91ffc7900d838
StasRomanov <stasromanov801@gmail.com>
2020-07-12 11:21:42 +0300

    fix-2 final 2

--- js/formFilter.js
+++ js/formFilter.js
@@ -24,6 +24,8 @@
   var elevatorFilter = checkboxFilter.querySelector('#filter-elevator');
   var conditionerFilter = checkboxFilter.querySelector('#filter-conditioner');
   var filterBlock = document.querySelector('.map__filters');
+  var filterBlockCheckbox = filterBlock.querySelectorAll('.map__checkbox');
+  var filterBlockSelect = filterBlock.querySelectorAll('.map__filter');
   var features = [wifiFilter.value, dishwasherFilter.value, parkingFilter.value, washerFilter.value, elevatorFilter.value, conditionerFilter.value];
   var featuresBlocks = [wifiFilter, dishwasherFilter, parkingFilter, washerFilter, elevatorFilter, conditionerFilter];
   var results = [];
@@ -98,13 +100,19 @@
     disable: function () {
       filterBlock.reset();
       filterBlock.classList.add('ad-form--disabled');
-      Array.from(filterBlock, function (item) {
+      filterBlockCheckbox.forEach(function (item) {
+        item.setAttribute('disabled', 'true');
+      });
+      filterBlockSelect.forEach(function (item) {
         item.setAttribute('disabled', 'true');
       });
     },
 
     enable: function () {
-      Array.from(filterBlock, function (item) {
+      filterBlockCheckbox.forEach(function (item) {
+        item.removeAttribute('disabled');
+      });
+      filterBlockSelect.forEach(function (item) {
         item.removeAttribute('disabled');
       });
       filterBlock.classList.remove('ad-form--disabled');
